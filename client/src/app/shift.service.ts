b7e10ca33ceaec5d1fa70d103b99441ad762f452
ProgrammerGimli <nicholas.taylor1410@gmail.com>
2017-08-29 17:15:57 +1000

    Blah

--- client/src/app/shift.service.ts
+++ client/src/app/shift.service.ts
@@ -9,9 +9,21 @@ export class ShiftService {
   constructor(private http: Http) { }
 
   //retrieve
-  getContacts() {
-    return this.http.get('http://localhost/3000/api/contacts')
+  getShifts() {
+    return this.http.get('http://localhost/3000/api/shifts')
       .map(res => res.json());
   }
-  //add
+  //add shift method
+  addShift(newShift){
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://://localhost/3000/api/shift', newShift, {headers:headers})
+      .map(res => res.json());
+  }
+
+  //delete shift method
+  deleteShift(id){
+    return this.http.delete("http://localhost/3000/api/shifts/" + id)
+      .map(res => res.json());
+  }
 }
--- client/src/app/shifts/shifts.component.ts
+++ client/src/app/shifts/shifts.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {ShiftService} from "../shift.service";
+import {Shift} from "../shift";
 
 @Component({
   selector: 'app-shifts',
   templateUrl: './shifts.component.html',
-  styleUrls: ['./shifts.component.css']
+  styleUrls: ['./shifts.component.css'],
+  providers: [ShiftService]
 })
 export class ShiftsComponent implements OnInit {
+  shifts: Shift[];
+  shift: Shift;
+  shift_giver:string;
+  shift_taker:string;
+  shift_date:string;
 
-  constructor() { }
+  constructor(private shiftService: ShiftService) { }
 
   ngOnInit() {
+    this.shiftService.getShifts()
+      .subscribe(shifts => 
+        this.shifts = shifts); 
   }
 
 }
